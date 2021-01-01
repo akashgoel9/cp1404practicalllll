5627c8455312d7959d8edde5975355e5aaac1eb9
ak <akas.goel@my.jcu.edu.au>
2021-01-01 22:47:42 +0530

    list comprehension

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -1,7 +1,4 @@
-"""
-CP1404/CP5632 Practical
-List comprehensions
-"""
+
 
 names = ["Bob", "Angel", "Jimi", "Alan", "Ada"]
 full_names = ["Bob Martin", "Angel Harlem", "Jimi Hendrix", "Alan Turing",
@@ -28,13 +25,21 @@ a_names = [name for name in names if name.startswith('A')]
 print(a_names)
 
 # TODO: use a list comprehension to create a list of all of the full_names
+
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
+
 # in lowercase format
 # lowercase_full_names =
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: use a list comprehension to create a list of integers
+numbers = [int(almost_number) for almost_number in almost_numbers]
+print(numbers)
 # from the above list of strings
 # numbers =
 
 # TODO: use a list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+big_numbers = [number for number in numbers if number > 9]
+print(big_numbers)
