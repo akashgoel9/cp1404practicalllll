630e1d39ae23d190d1fcbaecf82436146181b200
ak <akas.goel@my.jcu.edu.au>
2021-01-01 22:52:01 +0530

    list comprehension

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,33 @@
+Numbers = [3, 1, 4, 1, 5, 9, 2]
+
+
+
+print(Numbers[0])
+
+print(Numbers[-1])
+
+print(Numbers[3])
+
+print(Numbers[:-1])
+
+print(Numbers[3:4])
+
+print(5 in Numbers)
+
+print(7 in Numbers)
+
+print(Numbers + [6, 5, 3])
+
+# 4 exercises
+
+Numbers[0] = "ten"
+
+print(Numbers)
+
+Numbers[6] = 1
+
+print(Numbers)
+
+print(Numbers[2:])
+
+print(9 in Numbers)
--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,8 +1,3 @@
-"""
-CP1404/CP5632 Practical
-Data file -> lists program
-"""
-
 FILENAME = "subject_data.txt"
 
 
@@ -12,16 +7,16 @@ def main():
 
 
 def get_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
+        print(line)
+        print(repr(line))
+        line = line.strip()
+        parts = line.split(',')
+        print(parts)
+        parts[2] = int(parts[2])
+        print(parts)
         print("----------")
     input_file.close()
 
